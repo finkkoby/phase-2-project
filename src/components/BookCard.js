ea3905a129e5964b7817abdc03e37008aa58fcdc
Koby Fink <finkkoby@icloud.com>
2024-03-05 16:38:47 -0500

    inital books page

---
+++ src/components/BookCard.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+function BookCard({ book }) {
+    return (
+        <div className="card">
+            <div className='card-image'>
+                <img src={book.image} alt={book.title} />
+            </div>
+            <div className='card-info'>
+                <div className='card-info-title'>
+                    <h2>"{book.title}"</h2>
+                    <h5>by <em>{book.author}</em></h5>
+                </div>
+                <div className='card-info-body'>
+                    <small>{book.genre}</small>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BookCard;
--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from 'react';
+import BookCard from '../components/BookCard';
 function Books() {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/books`)
+       .then(res => res.json())
+       .then(books => setBooks(books));
+    }, []);
+
+    const booksList = books.map(book => {
+        return <BookCard book={book} key={book.id}/>
+    })
+
     return (
-        <div>
-            <h1>Books</h1>
+        <div className="item-list">
+            {booksList}
         </div>
     )
 }
