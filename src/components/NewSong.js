665553aa641e5c2bcd7cf9638c7c6e4c8bd9ee9e
Koby Fink <finkkoby@icloud.com>
2024-03-06 16:39:21 -0500

    complete song form

--- src/components/NewSong.js
+++ src/components/NewSong.js
@@ -1,23 +1,119 @@
 import { useState } from'react';
+import { Navigate } from 'react-router-dom';
 
 function NewSong() {
     const [title, setTitle] = useState('');
+    const [artist, setArtist] = useState('');
+    const [album, setAlbum] = useState('');
+    const [length, setLength] = useState('');
+    const [image, setImage] = useState('');
+    const [submited, setSubmitted] = useState(false);
+
+    function handleTitleChange(e) {
+        setTitle(e.target.value);
+    }
+
+    function handleArtistChange(e) {
+        setArtist(e.target.value);
+    }
+
+    function handleAlbumChange(e) {
+        setAlbum(e.target.value);
+    }
+
+    function handleLengthChange(e) {
+        setLength(e.target.value);
+    }
+
+    function handleImageChange(e) {
+        setImage(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const newSong = {
+            title: title,
+            artist: artist,
+            album: album,
+            length: length,
+            image: image
+        }
+
+        fetch('http://localhost:3000/songs', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newSong)
+        })
+      .then(res => res.json())
+      setSubmitted(true);
+    }
+
     return (
-        <form>
-            <label htmlFor='song-title'>Title: </label>
-            <input type="text" name="title" placeholder="Title" id='song-title' />
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='song-title'>
+                Title: 
+                <input 
+                type="text" 
+                name="title" 
+                placeholder="Title" 
+                value={title}
+                onChange={handleTitleChange}
+                id='song-title' 
+                />
+            </label>
 
-            <label htmlFor='song-artist'>Artist: </label>
-            <input type="text" name="artist" placeholder="Artist" id='song-artist' />
+            <label htmlFor='song-artist'>
+                Artist:             
+                <input 
+                type="text" 
+                name="artist" 
+                placeholder="Artist" 
+                value={artist}
+                onChange={handleArtistChange}
+                id='song-artist' 
+                />
+            </label>
 
-            <label htmlFor='song-album'>Album: </label>
-            <input type="text" name="album" placeholder="Album" id='song-album' />
+            <label htmlFor='song-album'>
+                Album: 
+                <input 
+                type="text" 
+                name="album" 
+                placeholder="Album" 
+                value={album}
+                onChange={handleAlbumChange}
+                id='song-album' 
+                />
+            </label>
 
-            <label htmlFor='song-length'>Duration: </label>
-            <input type="text" name="length" placeholder="Duration" id='song-length'/>
+            <label htmlFor='song-length'>
+                Duration: 
+                <input 
+                type="text" 
+                name="length" 
+                placeholder="Duration" 
+                value={length}
+                onChange={handleLengthChange}
+                id='song-length'
+                />
+            </label>
 
-            <label htmlFor='song-image'>Image: </label>
-            <input type="text" name="image" placeholder="Image" id='song-image'/>
+            <label htmlFor='song-image'>
+                Image:             
+                <input 
+                type="text" 
+                name="image" 
+                placeholder="Image" 
+                value={image}
+                onChange={handleImageChange}
+                id='song-image'
+                />
+            </label>
+            <button type="submit">Add new</button>
+            {submited? <Navigate to="/songs" /> : null}
         </form>
     )
 }
