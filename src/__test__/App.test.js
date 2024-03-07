c5132240f3c652b5f031074576c4d08b400e1f49
Koby Fink <finkkoby@icloud.com>
2024-03-07 12:17:08 -0500

    edited tests

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -2,18 +2,12 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from'react-router-dom';
 import App from '../App';
 
-test('renders NavBar on every page', () => {
-  render(<BrowserRouter><App /></BrowserRouter>);
-  const songsNav = screen.getByRole("link", { name: /Songs/i });
-  expect(songsNav).toBeInTheDocument();
-  const booksNav = screen.getByRole("link", { name: /Books/i });
-  expect(booksNav).toBeInTheDocument();
-  const tvMoviesNav = screen.getByRole("link", { name: /TV & Movies/i });
-  expect(tvMoviesNav).toBeInTheDocument();
-});
-
-test('renders h1 element on every page', () => {
-  render(<BrowserRouter><App /></BrowserRouter>);
-  const linkElement = screen.getByRole("heading", { name: /Koby's Current Favs/i });
+test('renders app', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByRole("heading", { name: /koby's current favs/i });
   expect(linkElement).toBeInTheDocument();
 })
--- src/__test__/Home.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from '../pages/Home';
-
-test('renders a welcome message', () => {
-    render(<Home />);
-    const linkElement = screen.getByRole("heading", { name: /welcome to my favorites!/i });
-    expect(linkElement).toBeInTheDocument();
-})
